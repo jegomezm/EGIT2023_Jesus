15895f42ceec74393ea637a06ab6a9ffe6296be6
Jesús <a_jesus.gomez.marquez@iespablopicasso.es>
2023-02-03 14:11:08 +0100

    Añadido el archivo Persona.java

---
+++ Persona.java
@@ -0,0 +1,18 @@
+package examenGIT;
+
+public class Persona {
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public Persona(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public void cumpleanios() {
+        edad++;
+        System.out.println("Tienes " + edad + " años");
+    }
+}
